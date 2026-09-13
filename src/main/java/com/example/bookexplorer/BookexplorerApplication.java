e2d438508797791c5303119c73234a5c0bdb8238
Seema-Prajapati21 <25104043@apsit.edu.in>
2026-09-13 18:59:47 +0530

    Add Spring Boot backend

---
+++ src/main/java/com/example/bookexplorer/BookexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookexplorerApplication.class, args);
+	}
+
+}
