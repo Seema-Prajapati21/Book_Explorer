5e5ea680a2fbd15a8c07fb5302d31f04516e93e6
Seema-Prajapati21 <25104043@apsit.edu.in>
2026-09-15 00:46:28 +0530

    Add user login endpoint

--- src/main/java/com/example/bookexplorer/UserController.java
+++ src/main/java/com/example/bookexplorer/UserController.java
@@ -1,4 +1,5 @@
 package com.example.bookexplorer;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.*;
@@ -13,29 +14,47 @@ public class UserController {
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-@PostMapping("/register")
-public User register(@RequestBody User user) {
 
-    if (userRepository.findByUsername(user.getUsername()).isPresent()) {
-        throw new ResponseStatusException(
-                HttpStatus.CONFLICT,
-                "Username already registered"
-        );
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Username already registered"
+            );
+        }
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Email already registered"
+            );
+        }
+
+        user.setRole("USER");
+
+        return userRepository.save(user);
     }
 
-    if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-        throw new ResponseStatusException(
-                HttpStatus.CONFLICT,
-                "Email already registered"
-        );
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+
+        User existingUser = userRepository
+                .findByUsername(user.getUsername())
+                .orElse(null);
+
+        if (existingUser != null &&
+            existingUser.getPassword().equals(user.getPassword())) {
+
+            return existingUser;
+        }
+
+        return null;
     }
 
-    user.setRole("USER");
-
-    return userRepository.save(user);
-}
-@GetMapping
-public java.util.List<User> getAllUsers() {
-    return userRepository.findAll();
-}
+    @GetMapping
+    public java.util.List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
